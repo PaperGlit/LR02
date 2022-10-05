26f04dc9fe9f110511bdb9e54ffa9d287e23a2b4
Glit <vpad.lazar.04@gmail.com>
2022-10-06 00:26:53 +0300

    The Finishing Touches

--- Lab_02/Lab_02.cpp
+++ Lab_02/Lab_02.cpp
@@ -14,26 +14,26 @@ int main()
 {
     SetConsoleOutputCP(1251);
 
-    double a;
-    double z1;
-    double z2;
+    double a; //Вхідне число
+    double z1; //Вихідне число #1
+    double z2; //Вихідне число #2
 
-    cout << "a="; cin >> a;
+    cout << "a="; cin >> a; //Уведення вхідного числа
 
-    z1 = (1 - 2 * sin(a) * sin(a)) / (1 + sin(2 * a));
-    z2 = (1 - tan(a)) / (1 + tan(a));
+    z1 = (1 - 2 * sin(a) * sin(a)) / (1 + sin(2 * a)); //Дія над числом #1
+    z2 = (1 - tan(a)) / (1 + tan(a)); //Дія над числом #2
 
-    cout << "z1=" << z1 << endl;
-    cout << "z2=" << z2 << endl;
+    cout << "z1=" << z1 << endl;  //Виведення вихідного числа #1
+    cout << "z2=" << z2 << endl;  //Виведення вихідного числа #2
 
-    if (fabs(z1 - z2) < 0.0000001)
+    if (fabs(z1 - z2) < 0.0000001) //Перевірка чи вихідні числа є рівними
     {
-        cout << "Вирази рівні" << endl;
+        cout << "Числа рівні" << endl;
     }
     else
     {
-        cout << "Вирази нерівні" << endl;
+        cout << "Числа нерівні" << endl;
     }
 
-    return 0;
+    return 0; //Кінець програми
 }
